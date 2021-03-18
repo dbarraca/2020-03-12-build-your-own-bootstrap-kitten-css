81ff8670cd7ae352c7d40680289a26f133c4ca8c
dbarraca <daniel.barraca@gmail.com>
2021-03-17 21:37:54 -0700

    Added some components

--- packages/byob-css-example/index.js
+++ packages/byob-css-example/index.js
@@ -1,1 +1,11 @@
 import "byob-css";
+
+
+let compCode = document.querySelectorAll('code');
+
+
+compCode.forEach((code) => {
+//   console.log(code);
+  let replaced = code.innerHTML.replaceAll('<','&lt;').replaceAll('>','&gt;');
+  code.innerHTML = replaced;
+});
